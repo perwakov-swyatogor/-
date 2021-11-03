44b091769c0fa5deac4c74dac30a43ecc9999f21
Перваков Святогор <92884843+perwakov-swyatogor@users.noreply.github.com>
2021-11-03 16:06:53 +0500

    Add files via upload

---
+++ lab.rab.3,Pervakov,prt-20-1b(1).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+#define PI 3.14159265 
+{
+    float an, x, y, a = 0.10, b = 0.80, sn = 0, se = 0, e = 0.0001, k = 10.00;
+    int n, m;
+    for (x = a; x <= b; x += (b - a) / k)
+    {
+        sn = 0;
+        se = 0;
+
+        for (n = 1; n <= 35; n++)
+        {
+            sn += (pow(x, n) * cos(n * 60 * PI / 180) / n);
+        };
+
+        m = 1;
+        an = 1;
+        while (an > e)
+        {
+            an = (pow(x, m) * cos(m * 60 * PI / 180) / m);
+            se += an;
+            m++;
+       } 
+
+        y = -0.5 * log(1 - 2 * x * cos(60 * PI / 180) + pow(x, 2));
+
+        printf("x=%.2f sn=%f se=%f y=%f\n", x, sn, se, y);
+    }
+
+    return 0;
+}
+
+
