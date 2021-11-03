b1ba710e3f09a190fc7d47c15696d292aafdd608
Перваков Святогор <92884843+perwakov-swyatogor@users.noreply.github.com>
2021-11-03 15:59:29 +0500

    Add files via upload

---
+++ Лабораторная 2/lab.rab.2(dowhile),Pervakov,PRT.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double eps, an, sum, k, n;
+	eps = 0.001;
+	sum = 0;
+	n = 0;
+	an = 1;
+	do
+	{
+		k = 2 * pow((n / (n + 1)), n);
+		an = an * k;
+		sum = sum + an;
+		n++;
+	} while (an >= eps);
+	cout << "Найти сумму ряда с точностью e=10-4, общий член которого an =  (2^n*n!)/n^n " << endl;
+	cout << "Ответ(сумма): " << sum << endl;
+	return 0;
+}
