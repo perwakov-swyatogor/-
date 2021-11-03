e92c11a409d7cbea0c97037d3d7579448540e11e
Перваков Святогор <92884843+perwakov-swyatogor@users.noreply.github.com>
2021-11-03 15:56:05 +0500

    Add files via upload

---
+++ Лабораторная 1/lab.rab.1(b),Pervakov, PRT-20-1b.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a, n, m;
+	cout << "введите n: ";
+	cin >> n;
+	cout << "введите m: ";
+	cin >> m;
+	a = --m - ++n;
+	cout << "Ответ(1): " << a << endl;
+
+	if (m * n < n++)
+		cout << "Ответ(2): Верно" << endl;
+	else
+		cout << "Ответ(2): Не верно" << endl;
+
+	if (n-- > m++)
+		cout << "Ответ(3): Верно" << endl;
+	else
+		cout << "Ответ(3): Не верно" << endl;
+
+	return 0;
+}
